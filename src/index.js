e2938bdaf434b9e713649f01d08a316893654606
briecodes <hello@brie.work>
2018-05-24 14:22:19 -0400

    first run through.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    const pokeDex = document.getElementById("pokemon-container");
+
+    let allPokemans = []
+
+    class Pokemon{
+        constructor(height, weight, order, name, abilities, moves, stats, types, sprites){
+            this.height = height;
+            this.weight = weight;
+            this.order = order;
+            this.name = name;
+            this.abilities = abilities;
+            this.moves = moves;
+            this.stats = stats;
+            this.types = types;
+            this.sprites = sprites;
+            allPokemans.push(this)
+        }
+        render(){
+            return `<li>
+            <img src="${this.sprites[0]}" />
+            ${this.name}
+            </li>`
+        }
+    }
+
+    function outputPokemans(array){
+        console.log(array);
+    }
+
+    function getPokemon(){
+        for (const mans of pokedex["pokemons"]){
+            let newPoke = new Pokemon(mans.height, mans.weight, mans.order, mans.name, mans.abilities, mans.moves, mans.stats, mans.types, mans.sprites)
+        }
+        debugger
+    }
+
+    getPokemon();
+
 })
